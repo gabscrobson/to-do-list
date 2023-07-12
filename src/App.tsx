28d8c368bb8fa3d758ec90b0ba66d925d5cd3283
gabscrobson <gmta1410@gmail.com>
2023-07-11 23:54:42 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,44 @@
+import { useState } from 'react'
 import styles from './App.module.css'
-import {PlusCircle} from 'phosphor-react'
 
 import { Header } from './components/Header'
+import { Input } from './components/Input'
+import { TaskList } from './components/TaskList'
+
+interface Task {
+  id: number;
+  content: string;
+  isCompleted: boolean;
+}
 
 function App() {
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function addTask(task: string) {
+    const newTask = {
+      id: tasks.length + 1,
+      content: task,
+      isCompleted: false
+    }
+
+    setTasks([newTask, ...tasks]);
+  }
+
+  function deleteTask(task: string) {
+    const newTasks = tasks.filter(taskItem => taskItem.content !== task);
+    setTasks(newTasks);
+  }
 
   return (
     <main>
       <Header />
+
       <div className={styles.wrapper}>
-
-        <form className={styles.inputForm}>
-          <input 
-            type='text' 
-            placeholder='Adicione uma nova tarefa' 
-            name='task'
-          />
-          <button type='submit'>
-            Criar <PlusCircle size={32} />
-          </button>
-        </form>
-
-
-
+        <Input onAddTask={addTask}/>
+        <TaskList tasks={tasks} onDeleteTask={deleteTask}/>
       </div>
+
     </main>
   )
 }
